e60184e43cf9779ec94cefe4725a67576b22cec6
Javacraft <frelling@java-craft.com>
2018-07-27 16:04:18 -0400

    Add Const and JVM arg selection

---
+++ src/main/java/jcc/dev/tag_dbg/Const.java
@@ -0,0 +1,12 @@
+package jcc.dev.tag_dbg;
+
+public class Const {
+    public static final String CMD_TAGTEST = "tagtest";
+    public static final String PROP_LOAD   = "tag-load";
+    public static final String PROP_ENABLE = "tag-enable";
+
+
+    private Const() {
+        throw new IllegalStateException("Utility class");
+    }
+}
--- src/main/java/jcc/dev/tag_dbg/TagDebug.java
+++ src/main/java/jcc/dev/tag_dbg/TagDebug.java
@@ -5,25 +5,51 @@ import java.util.logging.Logger;
 
 import org.bukkit.Material;
 import org.bukkit.Tag;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TagDebug extends JavaPlugin {
+public class TagDebug extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onLoad() {
-        testTag("ONLOAD", Tag.WOOL);
+
+        if (propSet(Const.PROP_LOAD))
+            testTag("ONLOAD", Tag.WOOL);
     }
 
 
     @Override
     public void onEnable() {
-        testTag("ONENABLE", Tag.WOOL);
+
+        if (propSet(Const.PROP_ENABLE))
+            testTag("ONENABLE", Tag.WOOL);
+
+        getCommand(Const.CMD_TAGTEST).setExecutor(this);
+    }
+
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command,
+            final String label, final String[] args) {
+
+        testTag("TESTDBG", Tag.WOOL);
+        return true;
+    }
+
+
+    private boolean propSet(final String propName) {
+
+        return System.getProperty(propName) != null;
     }
 
 
     private void testTag(final String phase, final Tag<Material> tag) {
+
         final Logger log = getLogger();
 
+        log.info(" ");
         Set<Material> mats = tag.getValues();
         if (mats == null)
             log.info(() -> quickFormat(phase, "Tag set is null"));
@@ -31,11 +57,12 @@ public class TagDebug extends JavaPlugin {
             log.info(() -> quickFormat(phase, "Tag set is EMPTY"));
         else
             log.info(() -> quickFormat(phase, mats.toString()));
-
+        log.info(" ");
     }
 
 
     private String quickFormat(final String phase, final String msg) {
+
         return "######## " + phase + " ######## : " + msg;
     }
 }
